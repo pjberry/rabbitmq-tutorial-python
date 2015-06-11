cf19bc53ae7edcbd3705b24af977e38119182e72
pjberry <pjberry@monsanto.com>
2015-06-11 16:54:59 -0500

    adding in ack

--- worker.py
+++ worker.py
@@ -7,12 +7,13 @@ def callback(ch, method, properties, body):
         print " [x] Received %r" % (body,)
         time.sleep( body.count('.') )
         print " [x] Done"
+        ch.basic_ack(delivery_tag = method.delivery_tag)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(callback, queue='hello')
 
 print ' [*] Waiting for messages. To exit, press CTRL+C'
 channel.start_consuming()
