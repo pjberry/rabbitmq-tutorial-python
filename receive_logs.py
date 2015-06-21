0f14ce2e38fd6d0920ad7820e381fbfab1678f71
pjberry <pjberry@monsanto.com>
2015-06-21 13:16:27 -0500

    adding in named parameters properlike

--- receive_logs.py
+++ receive_logs.py
@@ -5,20 +5,20 @@ import time
 
 def callback(ch, method, properties, body):
         print " [x] Received %r" % (body,)
-        time.sleep( body.count('.') )
-        print " [x] Done"
-        ch.basic_ack(delivery_tag = method.delivery_tag)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.exchange_declare('logs', type='fanout')
 
-channel.basic_qos(prefetch_count=1)
+channel.exchange_declare(exchange='logs', type='fanout')
+
 result = channel.queue_declare(exclusive=True)
-channel.queue_bind('logs', queue=result.method.queue)
+queue_name = result.method.queue
 
-channel.basic_consume(callback, queue='task_queue')
+channel.queue_bind(exchange='logs', queue=queue_name)
 
 print ' [*] Waiting for messages. To exit, press CTRL+C'
+
+channel.basic_consume(callback, queue=queue_name, no_ack=True)
+
 channel.start_consuming()
 
