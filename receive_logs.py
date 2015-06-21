8ac22fb3046c8149220e1d16a8bf65da8501c1db
pjberry <pjberry@monsanto.com>
2015-06-21 12:51:24 -0500

    added in exchange info

---
+++ receive_logs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import pika
+import time
+
+def callback(ch, method, properties, body):
+        print " [x] Received %r" % (body,)
+        time.sleep( body.count('.') )
+        print " [x] Done"
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.basic_qos(prefetch_count=1)
+channel.queue_declare(queue='task_queue', durable=True)
+channel.basic_consume(callback, queue='task_queue')
+
+print ' [*] Waiting for messages. To exit, press CTRL+C'
+channel.start_consuming()
+
